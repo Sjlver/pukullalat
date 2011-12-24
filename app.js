a618737a8c52aee73b6a9c6798bcd5790e2c5856
Jonas Wagner <j.b.w@gmx.ch>
2011-12-24 05:21:01 +0100

    Change port to 1612.

--- app.js
+++ app.js
@@ -75,5 +75,5 @@ app.post('/highscores', function(req, res) {
 
 // ***************************************************************************
 // Get going
-app.listen(3000);
+app.listen(1612);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
